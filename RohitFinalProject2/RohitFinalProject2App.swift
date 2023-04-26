cf5009129b325235554b2af1ed6293b290b4d96b
rohitdevdhar <rohit.devdhar15@gmail.com>
2023-04-26 15:47:48 -0700

    Initial Commit

---
+++ RohitFinalProject2/RohitFinalProject2App.swift
@@ -0,0 +1,17 @@
+//
+//  RohitFinalProject2App.swift
+//  RohitFinalProject2
+//
+//  Created by Rohit D on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct RohitFinalProject2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
